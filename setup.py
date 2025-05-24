cb159c204e00272c1527c8cfa14119d3a1d5d1af
Ningappa <ningskanavi89@gmail.com>
2025-05-24 13:08:16 +0530

    Saving changes before rebase

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
 
 setup(
+
     name='fruitpedia',
     version='0.1.1',
     description='A Python library for fruit information',
     packages=['fruitpedia'],
+
+    name="fruitpedia",
+    version="0.1.1",
+    author="Ningappa Kanavi",
+    description="A Python library with information on 100 fruits and CLI support",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/fruitpedia",
+    packages=find_packages(),
+    include_package_data=True,
+
     entry_points={
         'console_scripts': [
             'fruitpedia=fruitpedia.cli:main'
