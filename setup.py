8deb807b774b8463603dce4c251bb5506e9e713f
Ningappa <ningskanavi89@gmail.com>
2025-05-24 14:49:29 +0530

    Fix merge conflict markers in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fruitpedia',
-    version='0.1.1',
+    version='0.1.2',
     author='Ningappa Kanavi',
     description='A Python library with information on 100 fruits and CLI support',
     long_description=open("README.md").read(),
