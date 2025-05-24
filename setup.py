0c8eea3ee114f3cc926ef168af681e9a34562d34
Ningappa <ningskanavi89@gmail.com>
2025-05-24 14:41:18 +0530

    Fix merge conflict markers in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-
     name='fruitpedia',
     version='0.1.1',
-    description='A Python library for fruit information',
-    packages=['fruitpedia'],
-
-    name="fruitpedia",
-    version="0.1.1",
-    author="Ningappa Kanavi",
-    description="A Python library with information on 100 fruits and CLI support",
+    author='Ningappa Kanavi',
+    description='A Python library with information on 100 fruits and CLI support',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/fruitpedia",
-    packages=find_packages(),
+    packages=find_packages(),        # automatically find packages
     include_package_data=True,
-
     entry_points={
         'console_scripts': [
-            'fruitpedia=fruitpedia.cli:main'
-        ]
+            'fruitpedia=fruitpedia.cli:main',
+        ],
     },
-    install_requires=[],
+    install_requires=[],             # add dependencies here if any
 )
