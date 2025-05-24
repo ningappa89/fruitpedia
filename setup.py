cebf95925504a57cfc0e58ac97cca8dd390895a9
Ningappa <ningskanavi89@gmail.com>
2025-05-24 12:56:51 +0530

    Removed build and dist artifacts from Git tracking

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="fruitpedia",
-    version="0.1.1",
-    author="Ningappa Kanavi",
-    description="A Python library with information on 100 fruits and CLI support",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/fruitpedia",
-    packages=find_packages(),
-    include_package_data=True,
+    name='fruitpedia',
+    version='0.1.1',
+    description='A Python library for fruit information',
+    packages=['fruitpedia'],
     entry_points={
-        "console_scripts": [
-            "fruitpedia=fruitpedia.cli:main"
+        'console_scripts': [
+            'fruitpedia=fruitpedia.cli:main'
         ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
+    install_requires=[],
 )
