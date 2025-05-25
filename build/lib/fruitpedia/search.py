095955ea360de8b534f44696cd4817956318a83b
Ningappa <ningskanavi89@gmail.com>
2025-05-25 15:07:31 +0530

    Refactor code, clean build artifacts, add CLI test and CSV output

--- build/lib/fruitpedia/search.py
+++
@@ -1,4 +0,0 @@
-from .data import fruit_data
-
-def search_by_color(color):
-    return [name for name, info in fruit_data.items() if info["color"].lower() == color.lower()]
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 from fruitpedia.data import get_fruit_info
 
 def test_get_fruit_info():
     result = get_fruit_info("Banana")
     assert "potassium" in result["nutrients"]
-=======
-from fruitpedia.data import get_fruit_info
 
-def test_get_fruit_info():
-    result = get_fruit_info("Banana")
-    assert "potassium" in result["nutrients"]
->>>>>>> 496911e (Initial commit)
+
+
